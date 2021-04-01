13a0a4bbc25057523f6b3e5859b306e8b09b88b1
h3lpkey <willekb@gmail.com>
2021-04-01 09:40:00 +0500

    entry app page

--- fibberFrontend/src/pages/index.tsx
+++ fibberFrontend/src/pages/index.tsx
@@ -1,35 +1,45 @@
 import * as React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import QuestPreview from "../components/QuestPreview"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-const IndexPage = () => (
+import Logo from "../assets/images/logo.svg"
+
+const IndexPage = ({ data }) => (
   <Layout>
-    <div className="quests-preview-list">
-      <StaticQuery
-        query={graphql`
-          {
-            allStrapiQuest {
-              nodes {
-                questName
-                scene {
-                  id
-                }
-                id
-                questImagePreview {
-                  publicURL
-                }
-              }
-            }
-          }
-        `}
-        render={data =>
-          data.allStrapiQuest.nodes.map(quest => (
-            <QuestPreview key={quest.id} quest={quest} />
-          ))
-        }
-      />
+    <div className="page page-home">
+      <div className="logo">
+        <img src={Logo} alt="logo" />
+      </div>
+      <h1 className="title">Время охуительных историй</h1>
+      <p className="description">
+        Гарри услышал ее удаляющиеся шаги, а затем до него донесся звук
+        плюхнувшейся на плиту сковородки. Он перевернулся на спину и попытался
+        вспомнить, что же ему снилось.
+      </p>
+      <button
+        className="button-default button-default-big"
+        onClick={() => {
+          const link = `quest/${data.allStrapiQuest.nodes[0].id}/${data.allStrapiQuest.nodes[0].scene[0].id}`
+          window.location.href = link
+        }}
+      >
+        Начать игру
+      </button>
+      <button className="button-default">Начать заново</button>
+      <button className="button-default">Выйти из игры</button>
     </div>
   </Layout>
 )
-
+export const query = graphql`
+  {
+    allStrapiQuest {
+      nodes {
+        questName
+        scene {
+          id
+        }
+        id
+      }
+    }
+  }
+`
 export default IndexPage
